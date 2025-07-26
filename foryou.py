90f116af5a126bec1797e758b68d2874f6e6a61f
likthegod <najisebew@gmail.com>
2025-07-26 20:45:15 +0700

    index.html

--- foryou.py
+++ foryou.py
@@ -1,2 +1,17 @@
 <html>
+<head>
+  <style>
+    h1 {
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <h1>🖕🏼🖕🏼</h1>
+<img src="https://pin.it/U1EMoAfxL width="90%" height="auto">
+</body>
+<audio auto loop>
+<source src="
+<body>
+</body>
 </html>
